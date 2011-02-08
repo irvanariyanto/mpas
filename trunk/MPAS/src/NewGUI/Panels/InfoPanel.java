a2d013ac284599a3276718df491ae4bb9486e1be
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-02-08 18:09:29 +0000

    adding panels and new mainFrame

---
+++ trunk/MPAS/src/NewGUI/Panels/InfoPanel.java
@@ -0,0 +1,34 @@
+package NewGUI.Panels;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class InfoPanel extends JPanel {
+
+
+	private static final long serialVersionUID = 1L;
+	// Variables declaration 
+    private javax.swing.JLabel _lOpenList;
+    private javax.swing.JScrollPane _scrollPane;
+    private javax.swing.JTextArea _tOpenList;
+    // End of variables declaration
+    
+	public InfoPanel() {
+		initComponents();
+
+	}
+
+	private void initComponents() {
+		this.setBorder(BorderFactory.createTitledBorder("Information"));
+		_scrollPane = new JScrollPane();
+		_tOpenList = new JTextArea(5,20);
+	    _lOpenList = new JLabel("Open List:");
+	    _scrollPane.setViewportView(_tOpenList);
+	    this.add(_lOpenList);
+	    this.add(_scrollPane);
+
+	}
+}
