f98dc5cdce103c2c874d2cdf1f4f19648a6c6765
oferamit <oferamit@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-02-26 20:46:06 +0000

    controller constructor

--- MPAS/src/heuristics/ManhattanHeuristic.java
+++ MPAS/src/heuristics/ManhattanHeuristic.java
@@ -5,13 +5,26 @@ import heuristics.HeuristicInterface;
 import java.util.Vector;
 
 
+import algorithms.StateInterface;
 import algorithms.myPoint;
 import algorithms.myState;
 
-public class ManhattanHeuristic implements HeuristicInterface<myState> {
-
-	@Override
+public class ManhattanHeuristic implements HeuristicInterface<StateInterface<myPoint>> {
+/*
 	public float calcHeuristic(myState current, myState goal) {
+		Vector<myPoint> start = current.get_Coordinates();
+		Vector<myPoint> ends = goal.get_Coordinates(); 
+		float res = 0;
+		for (int i = 0;i < start.size();i++){
+			myPoint s = start.elementAt(i);
+			myPoint e = ends.elementAt(i);
+			res += (Math.abs(s.getX()-e.getX()) + Math.abs(s.getY()-e.getY()));
+		}
+		return res;
+	}
+*/
+	@Override
+	public float calcHeuristic(StateInterface<myPoint> current,	StateInterface<myPoint> goal) {
 		Vector<myPoint> start = current.get_Coordinates();
 		Vector<myPoint> ends = goal.get_Coordinates(); 
 		float res = 0;
