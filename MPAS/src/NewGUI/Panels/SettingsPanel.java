c51221eb8466961d87a92bb80ab528fc14c953e6
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-02 19:47:13 +0000

    adding the Density for the blocks

--- MPAS/src/NewGUI/Panels/SettingsPanel.java
+++ MPAS/src/NewGUI/Panels/SettingsPanel.java
@@ -19,6 +19,8 @@ public class SettingsPanel extends JPanel  {
 	private JButton _bCancel;
 	private JButton _bGeneratePositions;
 	private JButton _bClearPositions;
+	private JButton _bClearMap;
+	private JButton _bRandomMap;
 	private JComboBox _cAgents;
 	private JComboBox _cAlgorithm;
 	private JComboBox _cDirections;
@@ -31,12 +33,17 @@ public class SettingsPanel extends JPanel  {
 	private JLabel _lNumOfAgents;
 	private JLabel _lNumber;
 	private JLabel _lSize;
+	private JLabel _lDensity;
+    private JLabel _lDensityNumber;
 	private JRadioButton _rSetBlock;
 	private JRadioButton _rSetFinish;
 	private JRadioButton _rSetStart;
 	private JSlider _sNumOfAgents;
-	private JSeparator _separator;
+	private JSlider _sDensity;
 	private JSlider _sGridSize;	
+	private JSeparator _separator1;
+	private JSeparator _separator2;
+	
 	// End of variables declaration
 	/**
 	 * Constructor
@@ -67,15 +74,21 @@ public class SettingsPanel extends JPanel  {
 		_lSize = new JLabel("20 * 20");
 		_bApply = new JButton("Apply");
 		_bCancel = new JButton("Cancel");
-		_separator = new JSeparator();
+		_separator1 = new JSeparator();
+		_separator2 = new JSeparator();
 		_lAgents = new JLabel("Agent's number:");
 		_cAgents = new JComboBox();
 		_rSetStart = new JRadioButton("Start");
 		_rSetFinish = new JRadioButton("Finish");
 		_rSetBlock = new JRadioButton("Block");
 		_actionGroup = new ButtonGroup();
-		_bGeneratePositions= new JButton("Generate Positions");
-		_bClearPositions= new JButton("Clear All Positions");
+		_bGeneratePositions= new JButton("Random Positions");
+		_bClearPositions= new JButton("Clear Positions");
+		_bClearMap = new JButton("Clear Map");
+		_bRandomMap = new JButton("Random Map");
+		_lDensity = new JLabel("Density:");
+		_sDensity = new JSlider(0,100);
+		_lDensityNumber = new JLabel("50 %");
 
 		_cAlgorithm.setModel(new DefaultComboBoxModel(new String[] { "A-Star" }));
 		_cHeuristic.setModel(new DefaultComboBoxModel(new String[] { "Manhattan " }));
@@ -83,6 +96,7 @@ public class SettingsPanel extends JPanel  {
 		_cAgents.setModel(new DefaultComboBoxModel(new String[] { "1","2" }));
 		_sNumOfAgents.setValue(2);
 		_sGridSize.setValue(20);
+		_sDensity.setValue(50);
 		_actionGroup.add(_rSetBlock);
 		_actionGroup.add(_rSetStart);
 		_actionGroup.add(_rSetFinish);
@@ -106,6 +120,14 @@ public class SettingsPanel extends JPanel  {
 			}			
 		});
 		
+		_sDensity.addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent e) {
+				JSlider slider = (JSlider) e.getSource();
+				int value = slider.getValue();
+			    _lDensityNumber.setText(value + " %");				
+			}			
+		});
+		
  	
         
 		//setting the layout
@@ -114,9 +136,9 @@ public class SettingsPanel extends JPanel  {
 		layout.setHorizontalGroup(
 	            layout.createParallelGroup( GroupLayout.Alignment.LEADING)
 	            .addGroup(layout.createSequentialGroup()
+	                .addContainerGap()
 	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING)
 	                    .addGroup(layout.createSequentialGroup()
-	                        .addContainerGap()
 	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING)
 	                            .addComponent(_lAlgorithm)
 	                            .addComponent(_lDirections)
@@ -127,81 +149,86 @@ public class SettingsPanel extends JPanel  {
 	                            .addComponent(_cHeuristic, 0,  GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 	                            .addComponent(_cDirections, 0, 102, Short.MAX_VALUE)))
 	                    .addGroup(layout.createSequentialGroup()
-	                        .addContainerGap()
+	                        .addComponent(_lNumOfAgents)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                        .addComponent(_sNumOfAgents,  GroupLayout.PREFERRED_SIZE, 88,  GroupLayout.PREFERRED_SIZE)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                        .addComponent(_lNumber))
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_lGridSize)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                        .addComponent(_sGridSize,  GroupLayout.PREFERRED_SIZE, 116,  GroupLayout.PREFERRED_SIZE)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                        .addComponent(_lSize))
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_bApply)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                        .addComponent(_bCancel))
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_lAgents)
+	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                        .addComponent(_cAgents,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE))
+	                    .addGroup(layout.createSequentialGroup()
 	                        .addComponent(_rSetStart)
 	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
 	                        .addComponent(_rSetFinish)
 	                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
 	                        .addComponent(_rSetBlock))
-	                    .addGroup(layout.createSequentialGroup()
-	                        .addContainerGap()
-	                        .addComponent(_lAgents)
-	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                        .addComponent(_cAgents,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE))
-	                    .addGroup(layout.createSequentialGroup()
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
-	                            .addGroup( GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-	                                .addContainerGap()
-	                                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING, false)
-	                                    .addGroup(layout.createSequentialGroup()
-	                                        .addComponent(_lNumOfAgents)
-	                                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
-	                                        .addComponent(_sNumOfAgents,  GroupLayout.PREFERRED_SIZE, 88,  GroupLayout.PREFERRED_SIZE))
-	                                    .addGroup(layout.createSequentialGroup()
-	                                        .addComponent(_lGridSize)
-	                                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                                        .addComponent(_sGridSize, 0, 0, Short.MAX_VALUE))))
-	                            .addGroup( GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-	                                .addGap(45, 45, 45)
-	                                .addComponent(_bApply)
-	                                .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                                .addComponent(_bCancel)))
-	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING)
-	                            .addComponent(_lSize)
-	                            .addComponent(_lNumber)))
-	                    .addGroup(layout.createSequentialGroup()
-	                        .addContainerGap()
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING, false)
-	                            .addComponent(_bClearPositions,  GroupLayout.Alignment.LEADING,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	                            .addComponent(_bGeneratePositions,  GroupLayout.Alignment.LEADING,  GroupLayout.DEFAULT_SIZE,  GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-	                .addContainerGap())
-	            .addGroup(layout.createSequentialGroup()
-	                .addContainerGap()
-	                .addComponent(_separator,  GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE))
+	                    .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING, false)
+	                        .addComponent(_separator1,  GroupLayout.Alignment.LEADING,  GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+	                        .addGroup(layout.createSequentialGroup()
+	                            .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING)
+	                                .addGroup(layout.createSequentialGroup()
+	                                    .addComponent(_lDensity)
+	                                    .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                                    .addComponent(_sDensity,  GroupLayout.PREFERRED_SIZE, 78,  GroupLayout.PREFERRED_SIZE)
+	                                    .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                                    .addComponent(_lDensityNumber))
+	                                .addGroup(layout.createSequentialGroup()
+	                                    .addComponent(_bRandomMap,  GroupLayout.PREFERRED_SIZE, 138,  GroupLayout.PREFERRED_SIZE)
+	                                    .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                                    .addComponent(_bClearMap,  GroupLayout.PREFERRED_SIZE, 101,  GroupLayout.PREFERRED_SIZE)))
+	                            .addGap(26, 26, 26))
+	                        .addComponent(_separator2,  GroupLayout.Alignment.LEADING,  GroupLayout.PREFERRED_SIZE, 255,  GroupLayout.PREFERRED_SIZE)
+	                        .addGroup( GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+	                            .addComponent(_bGeneratePositions,  GroupLayout.PREFERRED_SIZE, 140,  GroupLayout.PREFERRED_SIZE)
+	                            .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
+	                            .addComponent(_bClearPositions))))
+	                .addContainerGap( GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 	        );
 	        layout.setVerticalGroup(
 	            layout.createParallelGroup( GroupLayout.Alignment.LEADING)
 	            .addGroup(layout.createSequentialGroup()
-	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
+	                    .addComponent(_lAlgorithm)
+	                    .addComponent(_cAlgorithm,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE))
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
+	                    .addComponent(_cHeuristic,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
+	                    .addComponent(_lHeuristic))
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
+	                    .addComponent(_cDirections,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
+	                    .addComponent(_lDirections))
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.LEADING)
 	                    .addGroup(layout.createSequentialGroup()
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
-	                            .addComponent(_lAlgorithm)
-	                            .addComponent(_cAlgorithm,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE))
-	                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
-	                            .addComponent(_cHeuristic,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
-	                            .addComponent(_lHeuristic))
-	                        .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
-	                            .addComponent(_cDirections,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
-	                            .addComponent(_lDirections))
-	                        .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                        .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
-	                            .addComponent(_sNumOfAgents,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
-	                            .addComponent(_lNumOfAgents)))
-	                    .addComponent(_lNumber))
-	                .addGap(17, 17, 17)
+	                        .addGap(11, 11, 11)
+	                        .addComponent(_lNumber))
+	                    .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
+	                        .addComponent(_sNumOfAgents,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
+	                        .addComponent(_lNumOfAgents)))
+	                .addGap(10, 10, 10)
 	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
 	                    .addComponent(_lGridSize)
 	                    .addComponent(_sGridSize,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
 	                    .addComponent(_lSize))
-	                .addGap(18, 18, 18)
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
 	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
 	                    .addComponent(_bApply)
 	                    .addComponent(_bCancel))
-	                .addPreferredGap( LayoutStyle.ComponentPlacement.UNRELATED)
-	                .addComponent(_separator,  GroupLayout.PREFERRED_SIZE, 10,  GroupLayout.PREFERRED_SIZE)
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                .addComponent(_separator1,  GroupLayout.PREFERRED_SIZE, 10,  GroupLayout.PREFERRED_SIZE)
 	                .addGap(1, 1, 1)
 	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
 	                    .addComponent(_lAgents)
@@ -212,20 +239,27 @@ public class SettingsPanel extends JPanel  {
 	                    .addComponent(_rSetFinish)
 	                    .addComponent(_rSetBlock))
 	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
-	                .addComponent(_bGeneratePositions)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
+	                    .addComponent(_bGeneratePositions)
+	                    .addComponent(_bClearPositions))
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+	                .addComponent(_separator2,  GroupLayout.PREFERRED_SIZE, 10,  GroupLayout.PREFERRED_SIZE)
 	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
-	                .addComponent(_bClearPositions)
-	                .addContainerGap( GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.TRAILING)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_lDensity)
+	                        .addGap(7, 7, 7))
+	                    .addComponent(_sDensity,  GroupLayout.PREFERRED_SIZE,  GroupLayout.DEFAULT_SIZE,  GroupLayout.PREFERRED_SIZE)
+	                    .addComponent(_lDensityNumber))
+	                .addPreferredGap( LayoutStyle.ComponentPlacement.RELATED)
+	                .addGroup(layout.createParallelGroup( GroupLayout.Alignment.BASELINE)
+	                    .addComponent(_bRandomMap)
+	                    .addComponent(_bClearMap))
+	                .addContainerGap())
 	        );
-	    }                               
-	
-	
+	    }
 
-
-    private void _bGeneratePositionsActionPerformed( ActionEvent evt) {
-        // TODO add your handling code here:
-    }
-    
+   
     /**
      * returns the Apply button
      * @return Apply button component
