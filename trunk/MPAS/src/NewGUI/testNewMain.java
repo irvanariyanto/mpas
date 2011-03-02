2c9149861d7659194abed3fb23c71f4aeac8bd1b
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-02 19:47:13 +0000

    adding the Density for the blocks

--- trunk/MPAS/src/NewGUI/testNewMain.java
+++ trunk/MPAS/src/NewGUI/testNewMain.java
@@ -11,7 +11,7 @@ public class testNewMain {
 	public static void main(String[] args) {
 		MainFrame2 main = new MainFrame2("MPAS - Multi-agent Pathfinding Algorithms Simulator v1.1");		
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setSize(new Dimension(890,700));
+		main.setSize(new Dimension(890,750));
 		main.setResizable(true);
 		main.setVisible(true);
 
