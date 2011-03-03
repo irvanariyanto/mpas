562a95b30084f5e383608824b17cd4697d91758e
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-03 20:01:28 +0000

    more functions for color cells Added

--- MPAS/src/NewGUI/Panels/SettingsPanel.java
+++ MPAS/src/NewGUI/Panels/SettingsPanel.java
@@ -1,10 +1,6 @@
 package NewGUI.Panels;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
