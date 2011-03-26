14fb8c5b1175f198df2b6d07025fa5cc430cb791
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-26 16:46:55 +0000

    slider for the auto step added, still have some bug in the layout when resizing the window

--- trunk/MPAS/src/Defaults/defaultsValues.java
+++ trunk/MPAS/src/Defaults/defaultsValues.java
@@ -2,6 +2,7 @@ package Defaults;
 
 public class defaultsValues {
 	
+	public static final int AutoStepTimer = 10;
 	public static int GridSize = 15;
 	public static int NumberOfAgents = 2;
 	public static int Density = 10;
