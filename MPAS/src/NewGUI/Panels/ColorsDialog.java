237e67dca165cd8ffb2236b3368e16f4debe4fcf
oferamit <oferamit@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-26 12:16:00 +0000

    Color manager addon

---
+++ MPAS/src/NewGUI/Panels/ColorsDialog.java
@@ -0,0 +1,30 @@
+package NewGUI.Panels;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class ColorsDialog {
+	/**
+	 * 
+	 */
+	private JDialog _root;
+	private static final long serialVersionUID = 1L;
+	public ColorsDialog(){
+		_root = new JDialog();
+		initComponents();
+	}
+	public ColorsDialog(JFrame frame){
+		_root = new JDialog(frame,"Color Manager",true);
+        initComponents();
+	}
+	private void initComponents() {
+		_root.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        _root.pack();
+        _root.setVisible(true);
+	}
+}
+
