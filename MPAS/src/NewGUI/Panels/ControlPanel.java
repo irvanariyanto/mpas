39fbd049067b8031d635bf182288717a9919c855
lironkatav <lironkatav@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-03-26 16:46:55 +0000

    slider for the auto step added, still have some bug in the layout when resizing the window

--- MPAS/src/NewGUI/Panels/ControlPanel.java
+++ MPAS/src/NewGUI/Panels/ControlPanel.java
@@ -3,10 +3,14 @@ package NewGUI.Panels;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JToggleButton;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-import com.sun.java.swing.plaf.nimbus.ToggleButtonPainter;
+import Defaults.defaultsValues;
 
 
 public class ControlPanel extends JPanel {
@@ -18,6 +22,9 @@ public class ControlPanel extends JPanel {
 	private  JButton _bStep;
 	private  JButton _bClearPath;
 	private JToggleButton _tbAutoStepMode;
+	private JSlider _sAutoStepMode;
+	private JLabel _lSec;
+	
 	// End of variables declaration
 
 	/**
@@ -39,33 +46,50 @@ public class ControlPanel extends JPanel {
 		_bStep = new JButton("Step");
 		_tbAutoStepMode = new JToggleButton("Auto");
 		_bClearPath = new JButton("Clear Path");
-		this.add(_bFindPath);
-		this.add(_bStop);
-		this.add(_bStep);
-		this.add(_bClearPath);
-		this.add(_tbAutoStepMode);
+		_sAutoStepMode = new JSlider(0,100);
+	    _lSec = new JLabel(defaultsValues.AutoStepTimer + " sec");
 		GroupLayout layout = new GroupLayout(this);
 		this.setLayout(layout);
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 		layout.setHorizontalGroup(
-				   layout.createSequentialGroup()
-				      .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				    		  .addComponent(_bFindPath)
-						      .addComponent(_bStop)
-						      .addComponent(_bStep)
-						      .addComponent(_bClearPath)
-						      .addComponent(_tbAutoStepMode))
-				);
-				layout.setVerticalGroup(
-				   layout.createSequentialGroup()
-				      .addComponent(_bFindPath)
-				      .addComponent(_bStop)
-				      .addComponent(_bStep)
-				      .addComponent(_bClearPath)
-				      .addComponent(_tbAutoStepMode)
-				);
-	}
+	            layout.createParallelGroup()
+	            .addGroup(layout.createSequentialGroup()
+	                .addGroup(layout.createParallelGroup()
+	                    .addComponent(_bFindPath)
+	                    .addComponent(_bStop)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_bStep)
+	                        .addComponent(_tbAutoStepMode)
+	                        .addComponent(_sAutoStepMode, GroupLayout.PREFERRED_SIZE, 88, GroupLayout.PREFERRED_SIZE)
+	                        .addComponent(_lSec))
+	                        .addComponent(_bClearPath)))
+	    );
+		layout.setVerticalGroup(
+				layout.createParallelGroup()
+	            .addGroup(layout.createSequentialGroup()
+	                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+	                    .addComponent(_lSec)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addComponent(_bFindPath)
+	                        .addComponent(_bStop)
+	                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+	                            .addComponent(_bStep)
+	                            .addComponent(_sAutoStepMode, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+	                            .addComponent(_tbAutoStepMode))))
+	                            .addComponent(_bClearPath))
+	    );
+		
+		_sAutoStepMode.setValue(defaultsValues.AutoStepTimer );
+		_sAutoStepMode.addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent e) {
+				JSlider slider = (JSlider) e.getSource();
+				int value = slider.getValue();
+			    _lSec.setText(value + " Sec");				
+			}			
+		});
+	    }
+	
 	public JToggleButton getAutoButton(){
 		return this._tbAutoStepMode;
 	}
