c5c2f72bee5181c70b421fb4bead2fde75180046
oferamit <oferamit@3146e38f-fbd5-5513-4cb1-fcade017ed89>
2011-04-14 19:32:43 +0000

    Implemented the Color Editor Dialog

---
+++ trunk/MPAS/src/GUI/Panels/ColorTableModel.java
@@ -0,0 +1,78 @@
+package GUI.Panels;
+
+import java.awt.Color;
+
+import javax.swing.table.AbstractTableModel;
+
+import GUI.Utils.ColorManager;
+
+public class ColorTableModel extends AbstractTableModel {
+    private String[] columnNames = {"Element",
+                                    "Color"};
+    private Object[][] data;
+
+    //constructor for the model, reads the color configuration from the colors.xml file
+    public ColorTableModel(){
+    	data = ColorManager.createDataModelFromFile();
+    }
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    public int getRowCount() {
+        return data.length;
+    }
+
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    public Object getValueAt(int row, int col) {
+        return data[row][col];
+    }
+
+    /*
+     * JTable uses this method to determine the default renderer/
+     * editor for each cell.  If we didn't implement this method,
+     * then the last column would contain text ("true"/"false"),
+     * rather than a check box.
+     */
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+    }
+
+    public boolean isCellEditable(int row, int col) {
+        //Note that the data/cell address is constant,
+        //no matter where the cell appears onscreen.
+        if (col < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+
+        data[row][col] = value;
+        if (col == 1){
+            ColorManager.setColor((String)data[row][0], (Color)value);
+        }
+
+        fireTableCellUpdated(row, col);
+
+    }
+
+    private void printDebugData() {
+        int numRows = getRowCount();
+        int numCols = getColumnCount();
+
+        for (int i=0; i < numRows; i++) {
+            System.out.print("    row " + i + ":");
+            for (int j=0; j < numCols; j++) {
+                System.out.print("  " + data[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------");
+    }
+}
--- trunk/MPAS/src/GUI/Panels/ColorsDialog.java
+++ trunk/MPAS/src/GUI/Panels/ColorsDialog.java
@@ -1,82 +1,59 @@
 package GUI.Panels;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
-public class ColorsDialog extends JDialog{
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	/**
+/**
+ * This is like TableDemo, except that it substitutes a
+ * Favorite Color column for the Last Name column and specifies
+ * a custom cell renderer and editor for the color data.
+ */
+public class ColorsDialog extends JDialog {
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JScrollPane _editorScrollPane;
-	private JEditorPane _editorPane;
-	private JButton _clearButton;
-	private JPanel _statsPanel;
-	private JPanel _buttonsPane;
-	public ColorsDialog(JFrame parent){
-		super(parent);
-		this.setTitle("Colors Editor");
+	private JPanel _mainPanel;
+    public ColorsDialog(JFrame frame) {
+    	super(frame);
+    	
+        _mainPanel = new JPanel(new GridLayout(1,0));
+        JTable table = new JTable(new ColorTableModel());
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        //Set up renderer and editor for the Favorite Color column.
+        table.setDefaultRenderer(Color.class,
+                                 new ColorRenderer(true));
+        table.setDefaultEditor(Color.class,
+                               new ColorEditor());
+
+        //Add the scroll pane to this panel.
+        _mainPanel.add(scrollPane);
+        this.setTitle("Colors Editor");
 		this.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
         this.pack();
-        this.setModal(true);
-        this.setSize(new Dimension(600,400));
-        _buttonsPane = new JPanel();
-
-        //editor pane
-        _editorPane = new JEditorPane();
-        //scroll pane
-        _editorScrollPane = new JScrollPane(_editorPane);
-        _editorScrollPane.setPreferredSize(new Dimension(250, 100));
-        _editorScrollPane.setAlignmentX(LEFT_ALIGNMENT);
-        _editorScrollPane.setVisible(true);
-        //main panel
-        _statsPanel = new JPanel();
-        _statsPanel.setLayout(new BoxLayout(_statsPanel, BoxLayout.PAGE_AXIS));
-        _statsPanel.add(Box.createRigidArea(new Dimension(0,5)));
-        _statsPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-        _statsPanel.add(_editorScrollPane);
-        //clear button
-        _clearButton = new JButton();
-        _clearButton.setText("Clear");
-        _clearButton.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				clearText();
-				
-			}
-		});
-     //   _statsPanel.add(_editorScrollPane);
-        _buttonsPane.add(_clearButton);
-        //Put everything together, using the content pane's BorderLayout.
+        this.setVisible(true);
         Container contentPane = getContentPane();
-        contentPane.add(_statsPanel, BorderLayout.CENTER);
-        contentPane.add(_buttonsPane, BorderLayout.PAGE_END);
-    //    this._editorScrollPane.add(_editorPane);
-        _editorPane.setText("Amitdsfdsdfs dsff  dgs\nds  dssdg");
-        _editorPane.setEditable(false);
-        _editorPane.setVisible(true);
+        contentPane.add(_mainPanel);
+        this.setSize(new Dimension(600,400));
+    }
+    
+    
 
-	}
-	public synchronized void addLine(String line){
-		String text = this._editorPane.getText();
-		this._editorPane.setText(text + "\n" + line);
-	}
-	public synchronized void clearText(){
-		_editorPane.setText("");
-	}
 }
